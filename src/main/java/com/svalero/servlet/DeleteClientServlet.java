f36905e647afb879f89800b26ea452b77f6c2ab5
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2023-05-16 22:18:47 +0200

    add: deletion functionality for clients

--- src/main/java/com/svalero/servlet/DeleteClientServlet.java
+++ src/main/java/com/svalero/servlet/DeleteClientServlet.java
@@ -1,4 +1,39 @@
 package com.svalero.servlet;
+import com.svalero.dao.ClientDAO;
+import com.svalero.dao.Database;
+import com.svalero.domain.Client;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
-public class DeleteClientServlet {
+@WebServlet("/delete-client")
+public class DeleteClientServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        String id = request.getParameter("id");
+
+        if (request.getSession().getAttribute("clients") != null) {
+            List<Client> clients = (List<Client>) request.getSession().getAttribute("clients");
+            request.getSession().removeAttribute("clients");
+            clients.removeIf(p -> p.getId().equals(id));
+            request.getSession().setAttribute("clients", clients);
+        }
+
+        try {
+            Database.connect();
+            Database.jdbi.withExtension(ClientDAO.class, dao -> {
+                dao.deleteClient(id);
+                return null;
+            });
+
+            response.sendRedirect("client-list.jsp");
+        } catch (ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+        }
+    }
 }
